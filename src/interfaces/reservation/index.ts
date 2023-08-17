2a8656f94bca188a5af5ce711d72ba65052cc284
roq-ai-snapshot <fabian.wesner@roq.tech>
2023-08-17 13:32:30 +0000

    Initial commit

---
+++ src/interfaces/reservation/index.ts
@@ -0,0 +1,25 @@
+import { UserInterface } from 'interfaces/user';
+import { TableInterface } from 'interfaces/table';
+import { RestaurantInterface } from 'interfaces/restaurant';
+import { GetQueryInterface } from 'interfaces';
+
+export interface ReservationInterface {
+  id?: string;
+  guest_id?: string;
+  table_id?: string;
+  restaurant_id?: string;
+  created_at?: any;
+  updated_at?: any;
+
+  user?: UserInterface;
+  table?: TableInterface;
+  restaurant?: RestaurantInterface;
+  _count?: {};
+}
+
+export interface ReservationGetQueryInterface extends GetQueryInterface {
+  id?: string;
+  guest_id?: string;
+  table_id?: string;
+  restaurant_id?: string;
+}
